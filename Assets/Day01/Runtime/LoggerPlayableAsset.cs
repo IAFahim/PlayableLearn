6ae4d7d013566d94752b3b51b5c7593fb9dfd9e8
vex.iafahim <iafahim.dev@gmail.com>
2026-01-23 19:13:23 +0600

    Day 01

--- Assets/Day01/Runtime/LoggerPlayableAsset.cs
+++ Assets/Day01/Runtime/LoggerPlayableAsset.cs
@@ -5,19 +5,13 @@ using UnityEngine.Timeline;
 namespace AV.Day01.Runtime
 {
     [System.Serializable]
-    public class LoggerClip : PlayableAsset, ITimelineClipAsset
+    public class LoggerPlayableAsset : PlayableAsset, ITimelineClipAsset
     {
-        public string message = "My Event Name";
-
         public ClipCaps clipCaps => ClipCaps.None;
 
         public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
         {
-            var playable = ScriptPlayable<LoggerBehaviour>.Create(graph);
-
-            var behaviour = playable.GetBehaviour();
-            behaviour.messageContent = message;
-
+            var playable = ScriptPlayable<LoggerPlayableBehaviour>.Create(graph);
             return playable;
         }
     }
---
+++ Assets/Day01/Runtime/LoggerPlayableBehaviour.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace AV.Day01.Runtime
+{
+    public class LoggerPlayableBehaviour : PlayableBehaviour
+    {
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            Debug.Log($"<color=green>Entered:</color> {info}");
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            Debug.Log($"<color=red>Left:</color> {info}");
+        }
+    }
+}
