374a7ed091f76d7b3fdf44ad4fc5dd4265dd774f
vex.iafahim <iafahim.dev@gmail.com>
2026-02-05 20:59:10 +0600

    beavior works

--- Assets/Day02/Runtime/Day02LoggerPlayableBehaviour.cs
+++ Assets/Day02/Runtime/Day02LoggerPlayableBehaviour.cs
@@ -5,5 +5,49 @@ namespace AV.Day02.Runtime
 {
     public class Day02LoggerPlayableBehaviour : PlayableBehaviour
     {
+        public override void OnGraphStart(Playable playable)
+        {
+            playable.LogFull();
+        }
+
+        public override void OnGraphStop(Playable playable)
+        {
+            playable.LogFull();
+        }
+
+        public override void OnPlayableCreate(Playable playable)
+        {
+            playable.LogFull();
+        }
+
+        public override void OnPlayableDestroy(Playable playable)
+        {
+            playable.LogFull();
+        }
+
+        public override void OnBehaviourPlay(Playable playable, FrameData info)
+        {
+            playable.LogFull(info, "green");
+        }
+
+        public override void OnBehaviourPause(Playable playable, FrameData info)
+        {
+            playable.LogFull(info, "red");
+        }
+
+        public override void PrepareData(Playable playable, FrameData info)
+        {
+            playable.LogFull(info, "yellow");
+        }
+
+        public override void PrepareFrame(Playable playable, FrameData info)
+        {
+            playable.LogFull(info, "orange");
+        }
+
+        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
+        {
+            playable.LogFull(info, "white", playerData);
+        }
     }
 }
