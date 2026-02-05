9268ff4a907575d3250f9b9366ce1e54d742621e
vex.iafahim <iafahim.dev@gmail.com>
2026-02-05 18:48:48 +0600

    Track switch

--- Assets/Day02/Runtime/Day02SystemTrack.cs
+++ Assets/Day02/Runtime/Day02SystemTrack.cs
@@ -1,10 +1,16 @@
+using UnityEngine;
+using UnityEngine.Playables;
 using UnityEngine.Timeline;
 
 namespace AV.Day02.Runtime
 {
-    [TrackBindingType(typeof(Day02Component))]
-    [TrackClipType(typeof(Day02PlayableAsset))]
+    [TrackColor(0.855f, 0.8623f, 0.87f)]
+    [TrackClipType(typeof(Day02LoggerPlayableAsset))]
     public class Day02SystemTrack : TrackAsset
     {
+        public override Playable CreateTrackMixer(PlayableGraph graph, GameObject go, int inputCount)
+        {
+            return ScriptPlayable<Day02MixerBehaviour>.Create(graph, inputCount);
+        }
     }
 }
--- Assets/Day03/Runtime/Day03VisualAttributesTrack.cs
+++ Assets/Day03/Runtime/Day03VisualAttributesTrack.cs
@@ -8,7 +8,6 @@ namespace AV.Day03.Runtime
     [TrackColor(0.5f, 0f, 0.5f)]
     [TrackBindingType(typeof(GameObject))]
     [TrackClipType(typeof(Day03DisplayNamePlayableAsset))]
-    [TrackClipType(typeof(Day02PlayableAsset))]
     [TrackClipType(typeof(AnimationPlayableAsset))]
     [DisplayName("Day 03/Visual Attributes Track")]
     public class Day03VisualAttributesTrack : TrackAsset
